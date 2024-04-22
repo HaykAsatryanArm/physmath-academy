fd0a3c44d85549a0a55ea19ca36d7f8bee3e88c7
HaykAsatryanArm <hayk.asatryan.arm@gmail.com>
2024-04-22 23:14:42 +0400

    Login/Registration validation with regex, error display + secret messages

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { SignIn, SignUp } from './Components'
 
 const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route exact path="/signup" element={<SignUp />} />
-        <Route exact path="/signin" element={<SignIn />} />
-      </Routes>
-    </Router>
-  )
+    return (
+        <Router>
+            <Routes>
+                <Route exact path="/signup" element={<SignUp />} />
+                <Route exact path="/signin" element={<SignIn />} />
+            </Routes>
+        </Router>
+    )
 }
 
 export default App
--- src/Components/SignIn/SignIn.jsx
+++ src/Components/SignIn/SignIn.jsx
@@ -1,25 +1,28 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { FaEye as Visible, FaEyeSlash as InVisible } from "react-icons/fa";
-
-import image from './../../Assets/Images/Account/signin_signup_vector.svg'
-
-import google from './../../Assets/Images/Account/google.svg'
-import facebook from './../../Assets/Images/Account/facebook.svg'
-import apple from './../../Assets/Images/Account/apple.svg'
-
-import styles from './SignIn.module.scss'
+import styles from './SignIn.module.scss';
+import image from './../../Assets/Images/Account/signin_signup_vector.svg';
+import google from './../../Assets/Images/Account/google.svg';
+import facebook from './../../Assets/Images/Account/facebook.svg';
+import apple from './../../Assets/Images/Account/apple.svg';
 
 const SignIn = () => {
+    const [formData, setFormData] = useState({ email: "", password: "" });
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+    const [errors, setErrors] = useState({});
 
-
-    const [changePassword, setChangePassword] = useState(true);
-    const changeIcon = changePassword === true ? false : true;
-
-
-    const dataSubmit = (event) => {
+    const dataSubmit = event => {
         event.preventDefault();
+        console.log(formData);
     };
+
+    const dataChange = event => {
+        const { name, value } = event.target;
+        setFormData(prevFormData => ({ ...prevFormData, [name]: value }));
+        setErrors(prevErrors => ({ ...prevErrors, [name]: "" }));
+    };
+
     return (
         <section className={styles.main}>
             <div className={styles.image}>
@@ -31,31 +34,45 @@ const SignIn = () => {
                         Մուտք Գործել
                     </h1>
                     <form className={styles.form} action="" method="POST" onSubmit={dataSubmit}>
-                        <h3 className={styles.formText}>Չունե՞ք պրոֆիլ <Link to={'/signup'}>Գրանցվել</Link></h3>
-                        <input className={styles.input} type="email" name="email" placeholder="Էլ․ Հասցե" required />
+                        <h3 className={styles.formText}>
+                            Չունե՞ք պրոֆիլ <Link to={'/signup'}>Գրանցվել</Link>
+                        </h3>
+                        <input
+                            className={`${styles.input} ${errors.email ? styles.active : ''}`}
+                            type="text" name="email"
+                            placeholder="Էլ․ Հասցե"
+                            onChange={dataChange}
+                            required />
+                        {errors.email && <div className={styles.errorMessage} title={errors.email}>{errors.email}</div>}
                         <div className={styles.password}>
-                            <input className={styles.input} type={changePassword ? "password" : "text"} name="password" placeholder="Գաղտնաբառ" required />
+                            <input className={`${styles.input} ${errors.password ? styles.active : ''}`}
+                                type={isPasswordVisible ? "text" : "password"}
+                                name="password"
+                                placeholder="Գաղտնաբառ"
+                                onChange={dataChange}
+                                required />
                             <span className={styles.vision}
                                 onClick={() => {
-                                    setChangePassword(changeIcon);
+                                    setIsPasswordVisible(!isPasswordVisible);
                                 }}
                             >
-                                {changeIcon ? <Visible /> : <InVisible />}
+                                {isPasswordVisible ? <Visible /> : <InVisible />}
                             </span>
                         </div>
+                        {errors.password && <div className={styles.errorMessage} title={errors.password}>{errors.password}</div>}
                         <button className={styles.submit} type="submit">Մուտք Գործել</button>
                     </form>
                     <div className={styles.or}>Կամ</div>
                     <div className={styles.options}>
-                        <a className={styles.icon} href="#"><img src={google} alt="" /> Շարունակել Google - ով</a>
-                        <a className={styles.icon} href="#"><img src={facebook} alt="" /> Շարունակել Facebook - ով</a>
-                        <a className={styles.icon} href="#"><img src={apple} alt="" /> Շարունակել Apple - ով</a>
+                        <a className={styles.icon} href="!#"><img src={google} alt="" /> Շարունակել Google-ով</a>
+                        <a className={styles.icon} href="!#"><img src={facebook} alt="" /> Շարունակել Facebook-ով</a>
+                        <a className={styles.icon} href="!#"><img src={apple} alt="" /> Շարունակել Apple-ով</a>
                     </div>
 
                 </div>
             </div>
-        </section>
-    )
+        </section >
+    );
 }
 
-export default SignIn
+export default SignIn;
